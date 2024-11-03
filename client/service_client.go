a3652d27bf6105451c866deba59bac863c143b7b
Ddarli <ivanlatyshev024@gmail.com>
2024-11-03 22:21:27 +0300

    feature

--- client/service_client.go
+++ client/service_client.go
@@ -32,3 +32,33 @@ func (sc *ServiceClient) GetServiceAddress(serviceName string) (string, error) {
 	service := services[0].Service
 	return fmt.Sprintf("http://%s:%d", service.Address, service.Port), nil
 }
+
+func (sc *ServiceClient) RegisterService(serviceName string, address string, port int) error {
+	// Создаем регистрацию сервиса
+	registration := &consulapi.AgentServiceRegistration{
+		ID:      fmt.Sprintf("%s-%s-%d", serviceName, address, port),
+		Name:    serviceName,
+		Address: address,
+		Port:    port,
+		Check: &consulapi.AgentServiceCheck{
+			HTTP:     fmt.Sprintf("http://%s:%d/health", address, port),
+			Interval: "10s",
+			Timeout:  "5s",
+		},
+		Tags: []string{"microservice", "api"},
+	}
+
+	// Регистрируем сервис
+	if err := sc.consul.Agent().ServiceRegister(registration); err != nil {
+		return fmt.Errorf("failed to register service: %w", err)
+	}
+
+	return nil
+}
+
+func (sc *ServiceClient) DeregisterService(serviceID string) error {
+	if err := sc.consul.Agent().ServiceDeregister(serviceID); err != nil {
+		return fmt.Errorf("failed to deregister service: %w", err)
+	}
+	return nil
+}
